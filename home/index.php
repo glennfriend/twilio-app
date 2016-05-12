629bac7c450b2185bd46c164d12f12bbadc6e7dc
root <root@training3.training3>
2016-05-12 01:53:14 -0700

    add layout to router

--- home/index.php
+++ home/index.php
@@ -5,9 +5,9 @@ initialize($basePath);
 $app = ProjectHelper::buildApp();
 
 // public
-$app->get('/',              'App\Controllers\Publicly\Home:defaultPage');
-$app->get('/status/{type}', 'App\Controllers\Publicly\Home:status');
-$app->get('/login',         'App\Controllers\Publicly\Auth:login');
-$app->post('/login',        'App\Controllers\Publicly\Auth:login');
+$app->get ('/',         'App\Controllers\Publicly\Home:defaultPage');
+$app->get ('/login',    'App\Controllers\Publicly\Auth:login');
+$app->post('/login',    'App\Controllers\Publicly\Auth:login');
+$app->get ('/logout',   'App\Controllers\Publicly\Auth:logout');
 
 $app->run();
